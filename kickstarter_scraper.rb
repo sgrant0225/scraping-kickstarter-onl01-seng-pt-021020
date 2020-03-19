c06029ed63084e2c3c1a0d6c969ce5aaca1b591e
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-19 00:07:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,7 +19,8 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text 
      projects[title.to_sym] = 
      {
-        :  
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value, 
+        
      }
  end
  projects
